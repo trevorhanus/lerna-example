22935abb350b60da63ee4df74896ea8eb887e0c4
Trevor Hanus <thanus@gravitypayments.com>
2018-05-09 14:29:52 -0700

    initial commit

---
+++ packages/main-app/src/index.ts
@@ -0,0 +1,6 @@
+import { generateSentence } from 'utils-one';
+
+const sentence = generateSentence();
+
+console.log('sentence from utils-one.');
+console.log(sentence);
---
+++ packages/utils-one/src/index.ts
@@ -0,0 +1,5 @@
+import * as casual from 'casual';
+
+export function generateSentence(): string {
+    return casual.sentence;
+}
